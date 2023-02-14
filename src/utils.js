10acf64be9609343644b793af995c5bbc6e2359a
Gabriel Godoy <gabyreload@gmail.com>
2023-02-14 18:08:00 -0300

    Resolving issue Imports duplicated

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,6 @@
 import { fileURLToPath } from "url";
-import { dirname } from "path";
+import path, { dirname } from "path";
 import fs from "fs";
-import path from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 
