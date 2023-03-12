59d39ca88d507c8c8793d92f498e00da28451760
Gabriel Godoy <gabyreload@gmail.com>
2023-03-12 11:24:38 -0300

    Add connection to mongodb and change imported routes

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ import express from "express";
 import { Server } from "socket.io";
 import handlebars from "express-handlebars";
 import { __dirname } from "./utils.js";
-import viewsRouter from "./routes/fileSystem/views.router.js";
-import productsRouter from "./routes/fileSystem/products.router.js";
-import cartsRouter from "./routes/fileSystem/carts.router.js";
+import viewsRouter from "./routes/web/views.router.js";
+import productsRouter from "./routes/api/products.router.js";
+import cartsRouter from "./routes/api/carts.router.js";
+import { mongoose } from "mongoose";
 
 const app = express();
 
@@ -20,6 +21,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 app.use("/", viewsRouter);
+
+try {
+  await mongoose.connect(
+    "mongodb+srv://gabriel:Jano1987@coderhouse.gszwtre.mongodb.net/ecommerce?retryWrites=true&w=majority"
+  );
+  console.log("Connected to Atlas mongoDB");
+} catch (error) {
+  console.log(error);
+}
 
 const server = app.listen(8080, () => console.log("Listening on port 8080"));
 
