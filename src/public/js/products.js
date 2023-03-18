3d1a2dd9b272ddb6a5f38817892caf46fb913fd3
Gabriel Godoy <gabyreload@gmail.com>
2023-03-18 17:55:46 -0300

    Adding sweetalert2

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -1,6 +1,7 @@
-const addToCart = async (id) => {
+const addToCart = async (pid) => {
+  const cid = "6416226fbc8cce33b3e09225";
   const response = await fetch(
-    `http://localhost:8080/api/carts/641525b808554bf135e5b8fe/product/${id}`,
+    `http://localhost:8080/api/carts/${cid}/product/${pid}`,
     {
       method: "POST",
       headers: {
@@ -8,4 +9,13 @@ const addToCart = async (id) => {
       },
     }
   );
+  if (response.status === 200)
+    Swal.fire({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 3000,
+      title: `Product added to cart`,
+      icon: "success",
+    });
 };
