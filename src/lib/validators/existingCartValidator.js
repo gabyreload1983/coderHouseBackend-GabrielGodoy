1456052099714f2d07700b3ec56f18f2b4a1e8cb
Gabriel Godoy <gabyreload@gmail.com>
2023-04-23 16:11:04 -0300

    delete validators files

--- src/lib/validators/existingCartValidator.js
+++
@@ -1,7 +0,0 @@
-const existingCartValidator = async (cartsManager, cid) => {
-  const cart = await cartsManager.getCart(cid);
-  if (!cart) throw new Error("That cart id does not exist");
-  return cart;
-};
-
-export default existingCartValidator;
