78c2cda5881308dd5e053058221ef6559516dd57
Gabriel Godoy <gabyreload@gmail.com>
2023-05-28 10:02:51 -0300

    styling sendEmailResetPassword view

--- src/public/js/sendEmailResetPassword.js
+++ src/public/js/sendEmailResetPassword.js
@@ -1,7 +1,12 @@
 const form = document.querySelector("#resetPasswordForm");
+const email = document.querySelector("#email");
+const btnRestPasswordEmail = document.querySelector("#btnRestPasswordEmail");
+const spinner = document.querySelector("#spinner");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  btnRestPasswordEmail.classList.add("disabled");
+  spinner.classList.remove("visually-hidden");
 
   const data = new FormData(form);
   const obj = {};
@@ -15,15 +20,20 @@ form.addEventListener("submit", (e) => {
       "Content-Type": "application/json",
     },
   }).then((result) => {
+    spinner.classList.add("visually-hidden");
     result.json().then((json) => {
       if (json.status === "success") {
+        email.value = "";
         Swal.fire({
           toast: true,
           position: "top-end",
           showConfirmButton: false,
-          timer: 1000,
+          timer: 2000,
           text: `${json.message}`,
           icon: "success",
+          willClose: () => {
+            window.location.replace("/login");
+          },
         });
       }
       if (json.status === "error") {
@@ -37,6 +47,7 @@ form.addEventListener("submit", (e) => {
           icon: "error",
         });
       }
+      btnRestPasswordEmail.classList.remove("disabled");
     });
   });
 });
