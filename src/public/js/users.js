f8241828f7c9413cac596cfaac59fc13afc61096
Gabriel Godoy <gabyreload@gmail.com>
2023-07-03 23:44:29 -0300

    Delete user button is added

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -46,6 +46,62 @@ form.addEventListener("submit", (e) => {
   });
 });
 
+const deleteUser = async (id) => {
+  const response = await Swal.fire({
+    title: `Do you want to delete the user ID:${id}`,
+    showConfirmButton: true,
+    confirmButtonText: "Delete",
+    showCancelButton: true,
+  });
+  if (response.isConfirmed) {
+    Swal.fire({
+      title: "Wait...",
+      html: "<strong>Wait...</strong>",
+      timerProgressBar: true,
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      showConfirmButton: false,
+      didOpen: () => {
+        Swal.showLoading();
+      },
+    });
+    const data = await fetch(`/api/users/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    const json = await data.json();
+    if (json.status === "success") {
+      Swal.fire({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        title: `${json.message}`,
+        icon: "success",
+        willClose: () => {
+          location.reload();
+        },
+      });
+    }
+    if (json.status === "error") {
+      Swal.fire({
+        toast: true,
+        position: "center",
+        showConfirmButton: false,
+        timer: 3000,
+        title: `Failed to delete user.`,
+        text: `${json.message}`,
+        icon: "error",
+        willClose: () => {
+          location.reload();
+        },
+      });
+    }
+  }
+};
+
 const getUser = async () => {
   const data = await fetch(`/api/users`, {
     method: "GET",
@@ -62,8 +118,16 @@ const getUser = async () => {
       <td>${user.last_name}</td>
       <td>${user.email}</td>
       <td>
-        ${user.role}
+      ${user.role}
       </td>
+      <td>
+        <button 
+          class="btn btn-sm btn-danger"
+          onclick="deleteUser('${user._id}')"
+        >
+          x
+        </button> 
+      </td>     
     `;
     listUsers.append(tr);
 
