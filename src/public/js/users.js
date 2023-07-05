bc81bf3a29fdf82bb353f13744589c61da4679b9
Gabriel Godoy <gabyreload@gmail.com>
2023-07-05 20:22:45 -0300

    delete inactive users router, controller and service

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -47,58 +47,136 @@ form.addEventListener("submit", (e) => {
 });
 
 const deleteUser = async (id) => {
-  const response = await Swal.fire({
-    title: `Do you want to delete the user ID:${id}`,
-    showConfirmButton: true,
-    confirmButtonText: "Delete",
-    showCancelButton: true,
-  });
-  if (response.isConfirmed) {
+  try {
+    const response = await Swal.fire({
+      title: `Do you want to delete the user ID:${id}`,
+      showConfirmButton: true,
+      confirmButtonText: "Delete",
+      showCancelButton: true,
+    });
+    if (response.isConfirmed) {
+      Swal.fire({
+        title: "Wait...",
+        html: "<strong>Wait...</strong>",
+        timerProgressBar: true,
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        showConfirmButton: false,
+        didOpen: () => {
+          Swal.showLoading();
+        },
+      });
+      const data = await fetch(`/api/users/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+      const json = await data.json();
+      if (json.status === "success") {
+        Swal.fire({
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 3000,
+          title: `${json.message}`,
+          icon: "success",
+          willClose: () => {
+            location.reload();
+          },
+        });
+      }
+      if (json.status === "error") {
+        Swal.fire({
+          toast: true,
+          position: "center",
+          showConfirmButton: false,
+          timer: 3000,
+          title: `Failed to delete user.`,
+          text: `${json.message}`,
+          icon: "error",
+          willClose: () => {
+            location.reload();
+          },
+        });
+      }
+    }
+  } catch (error) {
     Swal.fire({
-      title: "Wait...",
-      html: "<strong>Wait...</strong>",
-      timerProgressBar: true,
-      allowOutsideClick: false,
-      allowEscapeKey: false,
-      showConfirmButton: false,
-      didOpen: () => {
-        Swal.showLoading();
-      },
+      toast: true,
+      position: "center",
+      showConfirmButton: true,
+      title: `ERROR.`,
+      text: `${error.message}`,
+      icon: "error",
     });
-    const data = await fetch(`/api/users/${id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
+  }
+};
+
+const deleteInactiveUsers = async () => {
+  try {
+    const response = await Swal.fire({
+      title: `Do you want to delete all inactive users???`,
+      showConfirmButton: true,
+      confirmButtonText: "Delete",
+      showCancelButton: true,
     });
-    const json = await data.json();
-    if (json.status === "success") {
+    if (response.isConfirmed) {
       Swal.fire({
-        toast: true,
-        position: "top-end",
+        title: "Wait...",
+        html: "<strong>Wait...</strong>",
+        timerProgressBar: true,
+        allowOutsideClick: false,
+        allowEscapeKey: false,
         showConfirmButton: false,
-        timer: 3000,
-        title: `${json.message}`,
-        icon: "success",
-        willClose: () => {
-          location.reload();
+        didOpen: () => {
+          Swal.showLoading();
         },
       });
-    }
-    if (json.status === "error") {
-      Swal.fire({
-        toast: true,
-        position: "center",
-        showConfirmButton: false,
-        timer: 3000,
-        title: `Failed to delete user.`,
-        text: `${json.message}`,
-        icon: "error",
-        willClose: () => {
-          location.reload();
+      const data = await fetch(`/api/users`, {
+        method: "DELETE",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
         },
       });
+      const json = await data.json();
+      if (json.status === "success") {
+        Swal.fire({
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 3000,
+          title: `${json.message}`,
+          icon: "success",
+          willClose: () => {
+            location.reload();
+          },
+        });
+      }
+      if (json.status === "error") {
+        Swal.fire({
+          toast: true,
+          position: "center",
+          showConfirmButton: false,
+          timer: 3000,
+          title: `ERROR.`,
+          text: `${json.message}`,
+          icon: "error",
+          willClose: () => {
+            location.reload();
+          },
+        });
+      }
     }
+  } catch (error) {
+    Swal.fire({
+      toast: true,
+      position: "center",
+      showConfirmButton: true,
+      title: `ERROR.`,
+      text: `${error.message}`,
+      icon: "error",
+    });
   }
 };
 
@@ -113,13 +191,11 @@ const getUser = async () => {
   users.forEach((user) => {
     const tr = document.createElement("tr");
     tr.innerHTML = `
-      <td>${user._id}</td>
       <td>${user.first_name}</td>
       <td>${user.last_name}</td>
       <td>${user.email}</td>
-      <td>
-      ${user.role}
-      </td>
+      <td>${user.role}</td>
+      <td>${moment(user.last_connection).format("DD MM YYYY - h:mm:ss a")}</td>
       <td>
         <button 
           class="btn btn-sm btn-danger"
--- src/utils/htmlTemplates.js
+++ src/utils/htmlTemplates.js
@@ -14,3 +14,12 @@ export const resetPasswordHtml = (uid) => {
   <p>This link is valid for 1 hour</p>
   `;
 };
+
+export const userInactive = (user) => {
+  return `
+  <h2>Hello ${user.first_name} ${user.last_name}.</h2>
+  <p>Your acount was delete for inactivity</p>
+  <p>If you want to register again, please go to the next link</p>
+  <a href="http://localhost:8080/register">Register</a>
+    `;
+};
